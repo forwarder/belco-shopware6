4e2e6d6c2948290cbf6f6e8f4be4cdf904b7c301
Jorrit de Jong - JDJ-ICT <jorrit@jdjict.nl>
2020-06-19 16:39:38 +0200

    working
    without orders

--- Subscriber/BelcoSubscriber.php
+++ Subscriber/BelcoSubscriber.php
@@ -44,10 +44,153 @@ class BelcoSubscriber implements SubscriberInterface{
         $this->configReader = $configReader->getByPluginName('BelcoConnectorPlugin');
     }
 
+    
+
+    public function getCart() {
+        $cart = Shopware()->System()->sMODULES['sBasket']->sGetBasketData();
+
+        if (empty($cart['content'])) {
+            return null;
+        }
+
+        return array(
+            'total' => (float) $cart['AmountNumeric'],
+            'subtotal' => (float) $cart['AmountNetNumeric'],
+            'currency' => $this->currency,
+            'items' => array_map(function($item) {
+                return array(
+                'id' => $item['articleID'],
+                'name' => $item['articlename'],
+                'price' => (float) $item['priceNumeric'],
+                'url' => $item['linkDetails'],
+                'quantity' => (int) $item['quantity']
+                );
+            }, $cart['content'])
+        );
+    }
+
+    public function getCustomer() {
+        $data = Shopware()->System()->sMODULES['sAdmin']->sGetUserData();
+
+        $customer = array();
+
+        if (!empty($data['additional']['user'])) {
+            $user = $data['additional']['user'];
+
+            $customer = array(
+                'id' => $user['id'],
+                'firstName' => $user['firstname'],
+                'lastName' => $user['lastname'],
+                'email' => $user['email'],
+                'country' => $data['additional']['country']['countryiso'],
+                'signedUp' => strtotime($user['firstlogin'])
+            );
+
+            if ($data['billingaddress']['phone']) {
+                $customer['phoneNumber'] = $data['billing']['phone'];
+            }
+        }
+
+        return $customer;
+    }
+
+    private function getOrderData($customerId) {
+        $builder = Shopware()->Models()->createQueryBuilder();
+
+        $builder->select(array(
+            'SUM(orders.invoiceAmount) as totalSpent',
+            'MAX(orders.orderTime) as lastOrder',
+            'COUNT(orders.id) as orderCount',
+        ));
+
+        $builder
+            ->from('Shopware\Models\Order\Order', 'orders')
+            ->groupBy('orders.customerId')
+            ->where($builder->expr()->eq('orders.customerId', $customerId))
+            ->andWhere($builder->expr()->notIn('orders.status', array('-1', '4')))
+            ->addOrderBy('orders.orderTime', 'ASC');
+
+        $result = $builder->getQuery()->getOneOrNullResult();
+
+        if ($result) {
+            return array(
+                'totalSpent' => (float) $result['totalSpent'],
+                'lastOrder' => strtotime($result['lastOrder']),
+                'orderCount' => (int) $result['orderCount']
+            );
+        }
+    }
+
+    public function getWidgetConfig() {
+        /*
+        $configReader = $this->container->get('shopware.plugin.cached_config_reader')->getByPluginName('BelcoConnectorPlugin', $shop);
+
+        $shopId = $this->configReader['shopId'];
+        
+        /*
+        if (!$shopId) {
+            return;
+        }
+        */
+
+        $cart = Shopware()->System()->sMODULES['sBasket']->sGetBasketData();
+
+        $customer = $this->getCustomer(); 
+
+
+
+        $config = array(
+            'shopId' => $this->configReader['shopId'],
+            'hash' => hash_hmac('sha256', $this->getCustomer()['id'], $this->configReader['apiSecret']),
+            'id' => $this->getCustomer()['id'],
+            'email' => $this->getCustomer()['email'],
+            'firstName' => $this->getCustomer()['firstName'] ,
+            'lastName' => $this->getCustomer()['lastName'],
+            'signedUp' => $this->getCustomer()['signedUp'],
+            'cart' => $this->getCart()
+        );
+
+        if ($customer) {
+        $order = $this->getOrderData($customer['id']);
+
+        $belcoConfig = array_merge($config, $customer, $order);
+        }
+        
+
+        return json_encode($config);
+        /*
+        json_encode(
+            utf8_encode(
+                array_merge(
+                    $config, $customer, $order
+                )
+            )
+        );*/
+        
+        
+
+       
+        
+        
+/*
+        $view->assign('belcoConfig', $belcoConfig);
+        */
+    }
+    /*
+    private function createConfig() {
+        $this->Form()->setElement('text', 'shopId', array(
+            'label' => 'Shop Id'
+        ));
+
+        $this->Form()->setElement('text', 'apiKey', array(
+            'label' => 'Api Key'
+        ));
+    }*/
+
+    
+
     public function onPostDispatch(\Enlight_Controller_ActionEventArgs $args)
     { 
-        error_log($shopId, "shopid");
-        error_log($apiSecret, "apisecret");
 
         //deze is emptyyyyyyy
         /*$configReader = $this->container->get('shopware.plugin.cached_config_reader')->getByPluginName('BelcoConnectorPlugin', $shop);*/
@@ -60,33 +203,51 @@ class BelcoSubscriber implements SubscriberInterface{
         $shopId = $this->configReader['shopId'];
         $apiSecret = $this->configReader['apiSecret'];
 
+        error_log($shopId, "shopid");
+        error_log($apiSecret, "apisecret");
+
+        $belcoConfig = $this->getWidgetConfig();
+
         $view->assign('shopId', $shopId);
-        $view->assign('apiKey', $apiSecret);
+        $view->assign('apiSecret', $apiSecret);
+        $view->assign('belcoConfig', $belcoConfig);
+
+    
+        
         /*$shopId = 'G4ag3eGyiKibrGcM7';*/
 
         $view->addTemplateDir($this->pluginDirectory . '/Resources/views');
-        /*
-        $shop = $this->container->get('shop');
         
-        if (!$shop) {
-            $shop = $this->container->get('models')->getRepository(\Shopware\Models\Shop\Shop::class)->getActiveDefault();
-        }
-        */
-        /*
-        $belcoBackendData = $this->container->get('shopware.plugin.cached_config_reader')->getByPluginName('PluginName', $shop);
-        */
-        /*if ($belcoBackendData['shopId']) {
-            return;
-        }*/
-        
-        
-
         /*$view->assign('belcoConfig', $this->belcoConnector->getWidgetConfig());*/
         /*if (!$this->config['swagSloganContent']) {
             $view->assign('swagSloganContent', $this->sloganPrinter->getSlogan());
         }*/
     }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 
     public function install() {
